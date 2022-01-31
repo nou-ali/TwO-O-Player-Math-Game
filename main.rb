12b9004d838fdfbba3507fdf4a3f3ed6663ba015
vagrant <noura4403@gmail.com>
2022-01-31 21:41:22 +0000

    cleaned up code after looking at lecture notes

--- main.rb
+++ main.rb
@@ -2,10 +2,5 @@ require "./classes/player"
 require "./classes/question"
 require "./classes/game"
 
-math_game = Game.new
-
-game_questions = Question.new
-
-player1 = Player.new
-player2 = Player.new
-
+game1 = Game.new
+game1.play
