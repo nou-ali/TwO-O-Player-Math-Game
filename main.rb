2c87600587110f9d16100e23e1ecaabe9b16b7ef
vagrant <noura4403@gmail.com>
2022-01-31 21:28:44 +0000

    required classes and logic for game

---
+++ main.rb
@@ -0,0 +1,11 @@
+require "./classes/player"
+require "./classes/question"
+require "./classes/game"
+
+math_game = Game.new
+
+game_questions = Question.new
+
+player1 = Player.new
+player2 = Player.new
+
